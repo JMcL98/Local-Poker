1595e4a3fc13c92e3399c93ef96e70ec6a22ab1a
Jordan <jmclaughlin1998@gmail.com>
2020-01-28 16:19:12 +0000

    Implementation of Initialiser

--- app/src/main/java/com/e/localpoker/Card.java
+++ app/src/main/java/com/e/localpoker/Card.java
@@ -1,4 +1,24 @@
 package com.e.localpoker;
 
 class Card {
+    private char suit;
+    private int value;
+    private int index;
+    private boolean dealt;
+
+    public Card(char suit, int value, int index) {
+        this.suit = suit;
+        this.value = value;
+        this.index = index;
+        dealt = false;
+    }
+
+    public boolean checkDealtStatus() {
+        return dealt;
+    }
+
+    public Card dealCard() {
+        dealt = true;
+        return this;
+    }
 }
--- app/src/main/java/com/e/localpoker/Deck.java
+++ app/src/main/java/com/e/localpoker/Deck.java
@@ -1,4 +1,41 @@
 package com.e.localpoker;
 
-public class Deck {
+public class DeckManager {
+
+    private Card[] deck = null;
+    private boolean deckInitialised = false;
+
+    public DeckManager() {
+        deckInitialised = initialiseDeck();
+
+    }
+
+    private boolean initialiseDeck() {
+        int j = 2;
+        int i = 0;
+        while (i < 13) {
+            deck[i] = new Card('s', j, i);
+            j++;
+            i++;
+        }
+        j = 2;
+        while (i < 26) {
+            deck[i] = new Card('c', j, i);
+            j++;
+            i++;
+        }
+        j = 2;
+        while (i < 39) {
+            deck[i] = new Card('h', j, i);
+            j++;
+            i++;
+        }
+        j = 2;
+        while (i < 52) {
+            deck[i] = new Card('d', j, i);
+            j++;
+            i++;
+        }
+        return true;
+    }
 }
