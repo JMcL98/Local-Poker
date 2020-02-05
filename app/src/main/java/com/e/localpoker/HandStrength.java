6e5181c65719601b8e35ba27cb5db51b7ae7fe74
Jordan <jmclaughlin1998@gmail.com>
2020-02-05 15:18:43 +0000

    Added person class

--- app/src/main/java/com/e/localpoker/HandStrength.java
+++ app/src/main/java/com/e/localpoker/HandStrength.java
@@ -5,9 +5,37 @@ class HandStrength {
     int calculateStrength(Card[] hand) {
         int handStrength = 0;
 
-        
 
 
         return handStrength;
+    }
+
+    int checkFlush(Card[] hand) {
+        boolean flush = false;
+        
+    }
+
+    int checkStraight(Card[] hand) {
+
+    }
+
+    int checkFourOfAKind(Card[] hand) {
+
+    }
+
+    int checkFullHouse(Card[] hand) {
+
+    }
+
+    int checkThreeOfAKind(Card[] hand) {
+
+    }
+
+    int checkTwoPair(Card[] hand) {
+
+    }
+
+    int checkPair(Card[] hand) {
+
     }
 }
---
+++ app/src/main/java/com/e/localpoker/Player.java
@@ -0,0 +1,4 @@
+package com.e.localpoker;
+
+class Player {
+}
