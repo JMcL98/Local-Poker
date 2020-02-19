9ff7f35c541b709c7d435f01c7aceca28c3b3c68
Jordan <jmclaughlin1998@gmail.com>
2020-02-19 19:21:58 +0000

    Straight and Straight Flush checker implemented

--- app/src/main/java/com/e/localpoker/HandStrength.java
+++ app/src/main/java/com/e/localpoker/HandStrength.java
@@ -3,17 +3,21 @@ package com.e.localpoker;
 import android.util.Log;
 import android.widget.Switch;
 
-class HandStrength {
+final class HandStrength {
 
     static int calculateStrength(Card[] hand) {
-        int handStrength = 0;
-        if ((handStrength += checkFourOfAKind(hand)) > 1) {
-            return handStrength;
-        } else if ((handStrength += checkFlush(hand)) > 1) {
-            return handStrength;
+        int j;
+        int i = checkStraight(hand);
+        if (i >= 100000) {
+            return i;
+        } else if ((j = checkFourOfAKind(hand)) > 75000) {
+            return j;
+        } else if ((j = checkFlush(hand)) >= 50000) {
+            return j;
+        } else if (i >= 10000) {
+            return i;
         }
-
-        return handStrength;
+        return 0;
     }
 
     private static int checkFlush(Card[] hand) {
@@ -22,8 +26,8 @@ class HandStrength {
         int numSpades = 0;
         int numDiamonds = 0;
         int numClubs = 0;
-        for (int i = 0; i < 7; i++) {
-            char suit = hand[i].getSuit();
+        for (Card card : hand) {
+            char suit = card.getSuit();
             switch (suit) {
                 case 'h':
                     numHearts++;
@@ -44,33 +48,70 @@ class HandStrength {
         }
 
         if (flush) {
-            return 1;
+            return 50000;
         } else {
             return 0;
         }
     }
 
     private static int checkStraight(Card[] hand) {
-        // find lowest number, keep checking up until failure
-        // when 4 cards left no straight is possible
-        boolean lowestAce = false;
-        Card lowestCard;
-        lowestCard = hand[0];
-        for (int i = 1; i < hand.length; i++) {
-            if (hand[i].getValue() == 13) {
-                lowestAce = true;
-                lowestCard = hand[i];
-                break;
-            }
-            if (hand[i].getValue() < lowestCard.getValue()) {
-                lowestCard = hand[i];
+        Card[] orderedHand = orderHand(hand);
+        for (int i = 0; i < 3; i++) {
+            int j = nextStraightNumberCheck(1, orderedHand, i, 1);
+            if (j > 0) {
+                return j;
             }
         }
+        return 0;
+    }
 
+    private static Card[] orderHand(Card[] hand) {
+        for (int i = 0; i < hand.length; i++) {
+            for (int j = 0; j < hand.length - 1 - i; j++) {
+                if (hand[j].getValue() < hand[j+1].getValue()) {
+                    Card card = hand[j];
+                    hand[j] = hand[j+1];
+                    hand[j+1] = card;
+                }
+            }
+        }
+        return hand;
+    }
 
-
-
-
+    private static int nextStraightNumberCheck(int numFound, Card[] hand, int originalIndex, int flushCheck) {
+        for (Card card : hand) {
+            if (card.getValue() == hand[originalIndex].getValue() - numFound) {
+                if (numFound == 4) {
+                    if ((card.getSuit() == hand[originalIndex].getSuit()) && (flushCheck == 4)) {
+                        return hand[originalIndex].getValue() + 100000;
+                    } else {
+                        if (flushCheck == 4) {
+                            for (Card card2 : hand) {
+                                if ((card2.getValue() == card.getValue()) && (card2.getSuit() == hand[originalIndex].getSuit())) {
+                                    return hand[originalIndex].getValue() + 100000;
+                                }
+                            }
+                        }
+                        return hand[originalIndex].getValue() + 10000;
+                    }
+                } else {
+                    if (card.getSuit() == hand[originalIndex].getSuit()) {
+                        return nextStraightNumberCheck(numFound + 1, hand, originalIndex, flushCheck + 1);
+                    } else {
+                        if (flushCheck > 0) {
+                            for (Card card2 : hand) {
+                                if ((card2.getValue() == card.getValue()) && (card2.getSuit() == hand[originalIndex].getSuit())) {
+                                    return nextStraightNumberCheck(numFound + 1, hand, originalIndex, flushCheck + 1);
+                                }
+                            }
+                        }
+                        return nextStraightNumberCheck(numFound + 1, hand, originalIndex, 0);
+                    }
+                }
+            } else if ((hand[originalIndex].getValue() == 5) && (card.getValue() == 13) && (numFound == 4)) {
+                return hand[originalIndex].getValue();
+            }
+        }
         return 0;
     }
 
@@ -86,7 +127,7 @@ class HandStrength {
                 }
             }
             if (numMatches == 4) {
-                return 400 + v;
+                return 75000 + v;
             }
             numMatches = 1;
             i++;
@@ -97,7 +138,7 @@ class HandStrength {
 
     private static int checkFullHouse(Card[] hand) {
         // check three of a kind
-        // check pair
+        // check 2 pair
 
         return 0;
     }
